b17100bc37248545bf7bc9acbbbb615333a4dddc
isper3at <smith3at@gmail.com>
2017-12-28 15:55:28 -0500

    Added Auth based mongo test base
    This is a checkpoint commit since the
    test base in question is empty

---
+++ dao/mongodb.rya/src/test/java/org/apache/rya/mongodb/AuthenticatedMongoTestBase.java
@@ -0,0 +1,111 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.rya.mongodb;
+
+import java.util.List;
+
+import org.apache.hadoop.conf.Configuration;
+import org.bson.Document;
+import org.junit.Before;
+
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+
+/**
+ * A base class that may be used when implementing Mongo DB tests that use the
+ * JUnit framework.
+ */
+public class AuthenticatedMongoTestBase {
+
+    private MongoClient mongoClient = null;
+    protected StatefulMongoDBRdfConfiguration conf;
+
+    @Before
+    public void setupTest() throws Exception {
+        // Setup the configuration that will be used within the test.
+        final MongoDBRdfConfiguration conf = new MongoDBRdfConfiguration( new Configuration() );
+        conf.setBoolean("sc.useMongo", true);
+        conf.setTablePrefix("test_");
+        conf.setMongoDBName("testDB");
+        conf.setMongoHostname(EmbeddedMongoSingleton.getMongodConfig().net().getServerAddress().getHostAddress());
+        conf.setMongoPort(Integer.toString(EmbeddedMongoSingleton.getMongodConfig().net().getPort()));
+
+        // Let tests update the configuration.
+        updateConfiguration(conf);
+
+        // Create the stateful configuration object.
+        mongoClient = EmbeddedMongoSingleton.getNewMongoClient();
+        final List<MongoSecondaryIndex> indexers = conf.getInstances("ac.additional.indexers", MongoSecondaryIndex.class);
+        this.conf = new StatefulMongoDBRdfConfiguration(conf, mongoClient, indexers);
+
+        // Remove any DBs that were created by previous tests.
+        for(final String dbName : mongoClient.listDatabaseNames()) {
+            mongoClient.dropDatabase(dbName);
+        }
+    }
+
+    /**
+     * Override this method if you would like to augment the configuration object that
+     * will be used to initialize indexers and create the mongo client prior to running a test.
+     *
+     * @param conf - The configuration object that may be updated. (not null)
+     */
+    protected void updateConfiguration(final MongoDBRdfConfiguration conf) {
+        // By default, do nothing.
+    }
+
+    protected void addAdminUser(final String username, final char[] password) {
+
+    }
+
+    protected void addDBUser(final String username, final char[] password) {
+
+    }
+
+    protected void removeAdminuser(final String username) {
+
+    }
+
+    protected void removeDBUser(final String username) {
+
+    }
+
+
+    /**
+     * @return A {@link MongoClient} that is connected to the embedded instance of Mongo DB.
+     */
+    public MongoClient getMongoClient() {
+        return mongoClient;
+    }
+
+    /**
+     * @return The Rya triples {@link MongoCollection}.
+     */
+    public MongoCollection<Document> getRyaCollection() {
+        return mongoClient.getDatabase(conf.getMongoDBName()).getCollection(conf.getTriplesCollectionName());
+    }
+
+    /**
+     * @return The Rya triples {@link DBCollection}.
+     */
+    public DBCollection getRyaDbCollection() {
+        return mongoClient.getDB(conf.getMongoDBName()).getCollection(conf.getTriplesCollectionName());
+    }
+}
